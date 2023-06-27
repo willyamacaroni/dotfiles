e0d907b681f73133351c01d8fdf9e19c9c2af7c9
Willyam Arcand <willyam.arcand@shopify.com>
2023-06-27 10:15:54 -0400

    made my terminal a little less ugly

--- machine/ubuntu.sh
+++ machine/ubuntu.sh
@@ -6,15 +6,28 @@ sudo apt install curl neovim fzf rcm silversearcher-ag -y
 
 sudo apt-get install fuse libfuse2 git python3-pip ack-grep -y
 
-curl -sS https://starship.rs/install.sh | sh
+# curl -sS https://starship.rs/install.sh | sh
 
-# Install JetBrains Mono Nerd Font
-mkdir -p ~/.local/share/fonts
-cd ~/.local/share/fonts
-wget https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/FiraCode/Regular/complete/Fira%20Code%20Regular%20Nerd%20Font%20Complete.ttf
-fc-cache -fv
+# Install Nerd Font
 
+
+# Font installation script
+FONT_DIR="$HOME/.fonts"
+DOTFILES_FONT_DIR="$HOME/dotfiles/fonts"
+
+# Create the font directory if it doesn't exist
+mkdir -p "$FONT_DIR"
+
+# Copy the font files to the font directory
+cp "$DOTFILES_FONT_DIR"/*.otf "$FONT_DIR"
+
+# Update the font cache
+fc-cache -f -v
+
+# Install vim-plug
 curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
 
 # Run defaults.sh script from dotfiles
 bash ~/dotfiles/machine/ubuntu/defaults.sh
---
+++ zsh/configs/post/ohmyzsh.sh
@@ -0,0 +1,5 @@
+#THEME
+#ZSH_THEME="gozilla"
+
+#source ~/.oh-my-zsh/oh-my-zsh.sh
+
