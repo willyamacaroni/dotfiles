6d9dd255fcbd76eb173f63bfb05d1b68915aa61c
Willyam Arcand <willyam.arcand@shopify.com>
2023-06-27 13:42:42 -0400

    modify lead character

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -38,7 +38,7 @@ P.S. You can delete this when you're done too. It's your config now :)
 -- Set <space> as the leader key
 -- See `:help mapleader`
 --  NOTE: Must happen before plugins are required (otherwise wrong leader will be used)
-vim.g.mapleader = ','
+vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
 -- Install package manager
