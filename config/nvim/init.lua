16ff392477bf76934fac0577b17ef414025abb8c
Willyam Arcand <willyam.arcand@shopify.com>
2023-06-28 16:44:32 +0000

    changed indentblankline character

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -158,8 +158,10 @@ require('lazy').setup({
     -- Enable `lukas-reineke/indent-blankline.nvim`
     -- See `:help indent_blankline.txt`
     opts = {
-      char = '┊',
+      char = '|',
       show_trailing_blankline_indent = false,
+      show_current_context = true,
+      show_current_context_start = true,
     },
   },
 
