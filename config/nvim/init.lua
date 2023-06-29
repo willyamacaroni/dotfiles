5adf44132962c624f040220795f5a09507b88b29
Willyam Arcand <willyam.arcand@shopify.com>
2023-06-29 12:01:22 -0400

    remove dead code

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -41,9 +41,6 @@ P.S. You can delete this when you're done too. It's your config now :)
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
-local autocmd = vim.api.nvim_create_autocmd
-local augroup = vim.api.nvim_create_augroup
-
 -- Install package manager
 --    https://github.com/folke/lazy.nvim
 --    `:help lazy.nvim.txt` for more info
