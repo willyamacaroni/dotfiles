bb278ca4fbbb2096e9c62d2b91c445b2f889c36b
Willyam Arcand <willyam.arcand@shopify.com>
2023-07-01 18:57:15 -0400

    fix neovim on ubuntu

--- machine/ubuntu.sh
+++ machine/ubuntu.sh
@@ -6,10 +6,11 @@ sudo apt install curl neovim fzf rcm silversearcher-ag -y
 
 sudo apt-get install fuse libfuse2 git python3-pip ack-grep -y
 
-# curl -sS https://starship.rs/install.sh | sh
+# install starship
+curl -sS https://starship.rs/install.sh | sh
 
-# Install Nerd Font
-
+# install neovim
+$HOME/dotfiles/machine/spin/install_neovim.sh
 
 # Font installation script
 FONT_DIR="$HOME/.fonts"
@@ -23,11 +24,6 @@ cp "$DOTFILES_FONT_DIR"/*.otf "$FONT_DIR"
 
 # Update the font cache
 fc-cache -f -v
-
-# Install vim-plug
-curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-
-sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
 
 # Run defaults.sh script from dotfiles
 bash ~/dotfiles/machine/ubuntu/defaults.sh
