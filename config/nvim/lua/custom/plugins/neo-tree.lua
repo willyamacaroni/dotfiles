432c16e11a0ba94686b7ba3ec667781d5cb543a8
Willyam Arcand <willyam.arcand@shopify.com>
2023-07-11 21:12:12 +0000

    fixed neotree follow file

--- config/nvim/lua/custom/plugins/neo-tree.lua
+++ config/nvim/lua/custom/plugins/neo-tree.lua
@@ -1,15 +1,19 @@
 return {
   'nvim-neo-tree/neo-tree.nvim',
   dependencies = { "MunifTanjim/nui.nvim", "nvim-tree/nvim-web-devicons" },
+  branch = "v2.x",
   config = function()
-    require("neo-tree").setup({
-      default_component_config = {
-        filesystem = {
-          follow_current_file = true,
-          group_empty_dirs = true,
-          hijack_netrw_behavior = "open_default",
-        },
+    vim.cmd([[ let g:neo_tree_remove_legacy_commands = 1 ]])
+    local neotree = require 'neo-tree'
+    neotree.setup({
+      filesystem = {
+        follow_current_file = true,
+        group_empty_dirs = true,
+        hijack_netrw_behavior = "open_default",
       },
+      buffers = {
+        follow_current_file = true,
+      }
     })
     vim.cmd([[nnoremap <leader>rf :Neotree reveal<cr>]])
   end
