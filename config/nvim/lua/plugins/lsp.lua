86cb36f14b3d2e517fe39255485ba1997d00e002
Willyam Arcand <willyam.arcand@shopify.com>
2023-07-19 15:50:31 -0400

    added ruby lsp

---
+++ config/nvim/lua/plugins/lsp.lua
@@ -0,0 +1,60 @@
+return {
+    -- add ruby_ls and sorbet lsp to lspconfig
+  {
+    "neovim/nvim-lspconfig",
+    ---@class PluginLspOpts
+    opts = {
+      ---@type lspconfig.options
+      servers = {
+        -- ruby_ls and sorbet will be automatically installed with mason and loaded with lspconfig
+        ruby_ls = {},
+        sorbet = {},
+      },
+    },
+  },
+ 
+  -- add pyright to lspconfig
+  {
+    "neovim/nvim-lspconfig",
+    ---@class PluginLspOpts
+    opts = {
+      ---@type lspconfig.options
+      servers = {
+        -- pyright will be automatically installed with mason and loaded with lspconfig
+        pyright = {},
+      },
+    },
+  },
+
+  -- add tsserver and setup with typescript.nvim instead of lspconfig
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers ={
+        jsonls = {},
+        lua_ls = {
+          -- mason = false, -- set to false if you don't want this server to be installed with mason
+          -- Use this to add any additional keymaps
+          -- for specific lsp servers
+          ---@type LazyKeys[]
+          -- keys = {},
+          settings = {
+            Lua = {
+              workspace = {
+                checkThirdParty = false,
+              },
+              completion = {
+                callSnippet = "Replace",
+              },
+            },
+          },
+        },
+        ruby_ls = {},
+        sorbet = {}
+      },
+      setup = {
+
+      },
+    },
+  },
+}
--- config/nvim/lua/plugins/tree-sitter.lua
+++ config/nvim/lua/plugins/tree-sitter.lua
@@ -1,4 +1,4 @@
-{
+return {
   "nvim-treesitter/nvim-treesitter",
   opts = {
     ensure_installed = {
