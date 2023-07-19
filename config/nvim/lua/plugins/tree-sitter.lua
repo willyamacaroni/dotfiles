27eae232ca6753c53d5578d8c0559019bc1b030f
Willyam Arcand <willyam.arcand@shopify.com>
2023-07-19 10:47:51 -0400

    add ruby to treesitter

---
+++ config/nvim/lua/plugins/tree-sitter.lua
@@ -0,0 +1,26 @@
+{
+  "nvim-treesitter/nvim-treesitter",
+  opts = {
+    ensure_installed = {
+      "bash",
+      "c",
+      "html",
+      "javascript",
+      "json",
+      "lua",
+      "luadoc",
+      "luap",
+      "markdown",
+      "markdown_inline",
+      "python",
+      "query",
+      "regex",
+      "ruby",
+      "tsx",
+      "typescript",
+      "vim",
+      "vimdoc",
+      "yaml",
+    }
+  }
+}
