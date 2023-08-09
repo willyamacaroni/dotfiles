6e7d2020251bbdcf04880ba70dc08bb5c07c8ccd
Willyam Arcand <willyam.arcand@shopify.com>
2023-08-09 15:04:07 +0000

    fix spin ohmyzsh

--- machine/spin.sh
+++ machine/spin.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
 
 sudo apt-get install -y \
-  fzf \
-  rcm \
-  silversearcher-ag \
-  tmux
+	fzf \
+	rcm \
+	silversearcher-ag \
+	tmux
 
 # install neovim
 $HOME/dotfiles/machine/spin/install_neovim.sh
 
 # install starship
 curl -fsSL https://starship.rs/install.sh | sh -s -- -f
+
+sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+# Dracula
+ln -s $HOME/dotfiles/zsh/themes/dracula/dracula.zsh-theme $HOME/.oh-my-zsh/themes/dracula.zsh-theme
