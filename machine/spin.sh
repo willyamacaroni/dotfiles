27dcfb7ea99c5833f9c016b21b631935d29c06f0
Willyam Arcand <willyam.arcand@shopify.com>
2023-08-15 16:31:27 -0400

    fix spin ohmyzsh theme

--- machine/spin.sh
+++ machine/spin.sh
@@ -14,5 +14,8 @@ curl -fsSL https://starship.rs/install.sh | sh -s -- -f
 
 sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
+# Install theme
+sudo cp $HOME/dotfiles/zsh/themes/pixegami-agnoster.zsh-theme $HOME/.oh-my-zsh/themes/pixegami-agnoster.zsh-theme
+
 # Dracula
 ln -s $HOME/dotfiles/zsh/themes/dracula/dracula.zsh-theme $HOME/.oh-my-zsh/themes/dracula.zsh-theme
--- zsh/configs/post/ohmyzsh.sh
+++ zsh/configs/post/ohmyzsh.sh
@@ -3,7 +3,7 @@ os=$(uname -s)
 if [ "$os" = "Darwin" ]; then
 	ZSH_THEME="dracula"
 elif [ "$os" = "Linux" ]; then
-	ZSH_THEME="pixegami-agnoster"
+	ZSH_THEME="dracula"
 else
 	echo "Oh-my-zsh config| couldnt set theme | Unsupported operating system: $os"
 fi
