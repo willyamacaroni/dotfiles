3182e6008caf345870bf9a3014181be9deeaf923
Willyam Arcand <willyam.arcand@shopify.com>
2023-12-11 17:56:22 -0500

    tweaked telescope config

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -1,2 +1,5 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
+-- Make it obvious where 120 characters is
+vim.opt.textwidth = 80
+vim.opt.colorcolumn = "+1"
 require("config.lazy")
--- config/nvim/lua/plugins/telescope.lua
+++ config/nvim/lua/plugins/telescope.lua
@@ -1,18 +1,34 @@
 return {
   "telescope.nvim",
   dependencies = {
-    "nvim-telescope/telescope-fzf-native.nvim",
-    build = "make",
-    config = function()
-      require("telescope").load_extension("fzf")
-    end,
+    {
+      "nvim-telescope/telescope-fzf-native.nvim",
+      build = "make",
+    },
   },
-  setup = {
-    defaults = {
+  config = function(_, opts)
+    local telescope = require("telescope")
+
+    opts.defaults = vim.tbl_deep_extend("force", opts.defaults, {
+      wrap_results = true,
       layout_strategy = "vertical",
-      layout_config = {
-        vertical = { width = 0.99, height = 0.99 },
+      layout_config = { prompt_position = "bottom", height = 0.95, width = 0.95 },
+      sorting_strategy = "descending",
+      winblend = 0,
+      mappings = {
+        n = {},
+      },
+    })
+    opts.pickers = {
+      diagnostics = {
+        theme = "ivy",
+        initial_mode = "normal",
+        layout_config = {
+          preview_cutoff = 9999,
+        },
       },
     }
-  }
+    telescope.setup(opts)
+    require("telescope").load_extension("fzf")
+  end,
 }
