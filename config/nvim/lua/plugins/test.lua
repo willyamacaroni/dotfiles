2795ab0402acef871a58ddb4cc7c877d3eadaa4c
Willyam Arcand <willyam.arcand@shopify.com>
2023-12-19 10:46:04 -0500

    added mintiest adapter

--- config/nvim/lua/plugins/test.lua
+++ config/nvim/lua/plugins/test.lua
@@ -1,23 +1,16 @@
 return {
   {
     "nvim-neotest/neotest",
-    optional = true,
+    lazy = true,
     dependencies = {
-      "olimorris/neotest-rspec",
+      "zidhuss/neotest-minitest",
     },
-    opts = {
-      adapters = {
-        ["neotest-rspec"] = {
-          -- NOTE: By default neotest-rspec uses the system wide rspec gem instead of the one through bundler
-          -- rspec_cmd = function()
-          --   return vim.tbl_flatten({
-          --     "bundle",
-          --     "exec",
-          --     "rspec",
-          --   })
-          -- end,
+    config = function()
+      require("neotest").setup({
+        adapters = {
+          require("neotest-minitest"),
         },
-      },
-    },
+      })
+    end,
   },
 }
