9a22df89ae76c91188a85a0779ac1b4ff9d97e57
Willyam Arcand <willyam.arcand@shopify.com>
2023-12-19 10:39:20 -0500

    added neotest

---
+++ config/nvim/lua/plugins/test.lua
@@ -0,0 +1,23 @@
+return {
+  {
+    "nvim-neotest/neotest",
+    optional = true,
+    dependencies = {
+      "olimorris/neotest-rspec",
+    },
+    opts = {
+      adapters = {
+        ["neotest-rspec"] = {
+          -- NOTE: By default neotest-rspec uses the system wide rspec gem instead of the one through bundler
+          -- rspec_cmd = function()
+          --   return vim.tbl_flatten({
+          --     "bundle",
+          --     "exec",
+          --     "rspec",
+          --   })
+          -- end,
+        },
+      },
+    },
+  },
+}
