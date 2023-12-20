7e38c8904e6a583c97b3ab041f4fb16d939ddbf0
Willyam Arcand <willyam.arcand@shopify.com>
2023-12-20 15:32:25 -0500

    remove unused things

--- machine/spin.sh
+++ machine/spin.sh
@@ -10,12 +10,12 @@ sudo apt-get install -y \
 $HOME/dotfiles/machine/spin/install_neovim.sh
 
 # install starship
-curl -fsSL https://starship.rs/install.sh | sh -s -- -f
+# curl -fsSL https://starship.rs/install.sh | sh -s -- -f
 
-sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+# sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
 # Install theme
-sudo cp $HOME/dotfiles/zsh/themes/pixegami-agnoster.zsh-theme $HOME/.oh-my-zsh/themes/pixegami-agnoster.zsh-theme
+# sudo cp $HOME/dotfiles/zsh/themes/pixegami-agnoster.zsh-theme $HOME/.oh-my-zsh/themes/pixegami-agnoster.zsh-theme
 
 # Dracula
 ln -s $HOME/dotfiles/zsh/themes/dracula/dracula.zsh-theme $HOME/.oh-my-zsh/themes/dracula.zsh-theme
